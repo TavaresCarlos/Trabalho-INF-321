0e861d2f0f9556765a5c6ce179e21f90d4f5d127
TavaresCarlos <caca_cht@hotmail.com>
2019-11-17 10:01:50 -0300

    Home do sistema

--- view/js/pagina-inicial.js
+++ view/js/pagina-inicial.js
@@ -1,6 +1,18 @@
 function home(){
 	var menu = document.getElementById("menu");
 	menu.innerHTML =`
-		DDDDDDDDDDDDDDDDDDDDD
+		<div class="container">
+		  <div class="row">
+		    <div class="col-xl-3">
+		    </div>
+		    <div class="col-xl-6">
+		      <p>BEM-VINDO A <strong>AGENDA C&H</strong>, SUA FÁCIL, RÁPIDA E CUSTOMIZÁVEL AGENDA PESSOAL.</p>
+		      <p>SE VOCÊ JÁ É UM DOS NOSSOS PARCEIROS, CLIQUE <a href="">AQUI</a>.</p>
+		      <p>SENÃO, EFETUE UM CADASTRO RÁPIDO E FÁCIL CLICANDO <a href="">AQUI</a>.</p>
+		    </div>
+		    <div class="col-xl-3">
+		    </div>
+		  </div>
+		</div>
 	`;
 }
