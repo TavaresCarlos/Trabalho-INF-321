ad25fcf8f70752aaa52b0c2f65e7d10d020a9db7
TavaresCarlos <caca_cht@hotmail.com>
2019-11-18 14:37:15 -0300

    Inicio da implementação da tela de cadastro do novo usuário

---
+++ view/js/cadastro.js
@@ -0,0 +1,35 @@
+function cadastro(){
+	var cadastro = document.getElementById("menu");
+	cadastro.innerHTML = `
+		<div class="row">
+			<div class="col-xl-3">
+		    </div>
+		    <div class="col-xl-6">
+		      <form method="POST" action="" id="formulario-cadastro">
+				<fieldset>
+					<legend>Dados cadastrais</legend>
+						<label for="exampleInputEmail1">Nome completo</label>
+			    			<input type="text" class="form-control" id="nome" name="name" aria-describedby="emailHelp" placeholder="Nome completo">
+			    		<label for="exampleFormControlSelect1">Sexo</label>
+						    <select class="form-control" id="exampleFormControlSelect1">
+						    	<option value="null">---------</option>
+						     	<option value="masculino">Masculino</option>
+						     	<option value="feminino">Feminino</option>
+						     	<option value="nao informado">Não informar</option>
+			    			</select>
+		    	</fieldset>
+		    	<fieldset>
+		    		<legend>Endereço</legend>
+		    	</fieldset>
+		    	<fieldset>
+		    		<legend>Contato</legend>
+		    	</fieldset>
+		    	<button type="submit" class="btn btn-primary">Cadastrar</button>
+				<button type="submit" class="btn btn-primary" onclick="home()">Voltar</button>
+			</form>
+		    </div>
+		    <div class="col-xl-3">
+		    </div>
+		</div>
+	`;
+}
--- view/js/pagina-inicial.js
+++ view/js/pagina-inicial.js
@@ -8,7 +8,7 @@ function home(){
 		    <div class="col-xl-6">
 		      <p>BEM-VINDO A <strong>AGENDA C&H</strong>, SUA FÁCIL, RÁPIDA E CUSTOMIZÁVEL AGENDA PESSOAL.</p>
 		      <p>SE VOCÊ JÁ É UM DOS NOSSOS PARCEIROS, CLIQUE<button type="button" class="btn btn-link" onclick="login()">AQUI</button></p>
-		      <p>SENÃO, EFETUE UM CADASTRO RÁPIDO E FÁCIL CLICANDO<button type="button" class="btn btn-link">AQUI</button></p>
+		      <p>SENÃO, EFETUE UM CADASTRO RÁPIDO E FÁCIL CLICANDO<button type="button" class="btn btn-link" onclick="cadastro()">AQUI</button></p>
 		    </div>
 		    <div class="col-xl-3">
 		    </div>
