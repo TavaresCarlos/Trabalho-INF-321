c53820649cee58bf1a58e6b713855cd18c591f96
TavaresCarlos <caca_cht@hotmail.com>
2019-11-22 17:17:54 -0300

    Drag and drop adicionado

--- view/js/login.js
+++ view/js/login.js
@@ -1,7 +1,7 @@
 function login(){
 	var menu = document.getElementById("menu");
 	menu.innerHTML = `	
-		<div class="container" id="draggable">
+		<div class="container">
 			<form method="POST" action="">
 			 <div class = row>
 			 	<div class="col-xl-4" id="left">
