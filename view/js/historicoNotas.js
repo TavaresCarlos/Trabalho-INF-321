a389978b739c850f5aa22b36157bf5d5aa2f4800
TavaresCarlos <caca_cht@hotmail.com>
2019-11-22 13:41:03 -0300

    Menu superior pós login e card de histórico adicionados

---
+++ view/js/historicoNotas.js
@@ -0,0 +1,18 @@
+function historico(){
+	var menu = document.getElementById('painel-secundario');
+	menu.innerHTML = `
+		<div class="cardNota" style="padding-left: 1%; padding-top: 1%;">
+			<div class="card" style="width: 18rem;">
+			  <img class="card-img-top" src="img/1.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h5 class="card-title">Titulo do card</h5>
+			    <p class="card-text">Descrição</p>
+			    <div class="card-body">
+		    		#HishTags
+		  		</div>
+			    <button type="button" class="btn btn-primary" onclick="">Ver anotação</button>
+			  </div>
+			</div>
+		</div>
+	`;
+}
