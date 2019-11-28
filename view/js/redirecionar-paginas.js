57adb67db46a479a4134ea6ba570e813966d8e9c
TavaresCarlos <caca_cht@hotmail.com>
2019-11-28 13:15:21 -0300

    Implementado .js de redirecionamento das paginas do sistema

---
+++ view/js/redirecionar-paginas.js
@@ -0,0 +1,17 @@
+//arquivo .js responsável pelo redirecionamento das páginas no sistema
+
+function home(){
+	window.location.href = "home.html";
+}
+
+function historico(){
+	window.location.href = "historico.html";
+}
+
+function nota(){
+	window.location.href = "nova-nota.html";
+}
+
+function logout(){
+	window.location.href = "index.html";
+}
