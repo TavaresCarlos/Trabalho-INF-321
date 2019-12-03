6f44471a28f6b0036cdc783cffbe79ce75e4c6a6
TavaresCarlos <caca_cht@hotmail.com>
2019-12-03 10:29:10 -0300

    Inicio da implementação do model.php

--- model/model.php
+++ model/model.php
@@ -3,7 +3,88 @@
 	include '../controler/controler.php';
 	include '../controler/conexao.php';
 
-	class model{
+	class usuario{
+		private $nome;
+		private $email;
+		private $senha;
+		private $tipoUsuario;
+
+		function construtorUsuario($name, $email, $password, $typeUser){
+			$this->nome = $name;
+			$this->email = $email;
+			$this->senha = $password;
+			$this->tipoUsuario = $typeUser;
+		}
+		function setNome($name){	
+			$this->nome = $name;
+		}
+		function setEmail($email){
+			$this->email = $email;
+		}
+		function setSenha($password){
+			$this->senha = $password;
+		}
+		function setTipoUsuario($typeUser){
+			$this->tipoUsuario = $typeUser;
+		}
+		function getNome(){
+			return $nome;
+		}
+		function getEmail(){
+			return $email;
+		}
+		function getSenha(){
+			return $senha;
+		}
+		function getTipoUsuario(){
+			return $tipoUsuario;
+		}
+
+		//Funções para persistência no banco de dados
+
+
+
+	}
+
+	class notas{
+		private $titulo;
+		private $categoria;
+		private $prioridade;
+		private $descricao;
+
+		function construtorNotas($title, $category, $priority, $description){
+			$this->titulo = $title;
+			$this->categoria = $category;
+			$this->prioridade = $priority;
+			$this->descricao = $description;
+		}
+		function setTtitulo($title){
+			$this->titulo = $title;
+		}
+		function setCategoria($category){
+			$this->categoria = $category;
+		}
+		function setPrioridade($priority){
+			$this->prioridade = $priority;
+		}
+		function setDescricao($description){
+			$this->descricao = $description
+		}
+		function getTtitulo(){
+			return $titulo;
+		}
+		function getCategoria(){
+			return $categoria;
+		}
+		function getPrioridade(){
+			return $prioridade;
+		}
+		function getDescricao(){
+			return $descricao
+		}
+
+		//Funções para persistência no banco de dados
+
 
 	}
 
